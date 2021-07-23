a17de21b30eb8100b9763c45435f3fabb5d845ea
Lelton Borges <leltondev@gmail.com>
2021-07-23 20:34:10 -0300

    Retirando erro

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -22,7 +22,7 @@ public class DateUtilsTest {
 	@Test
 	public void deverRetornarTrueParaDatasAtual() {
 		LocalDate date = LocalDate.now();
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 	}
 
 }
